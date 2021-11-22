474629f35852c8942e5374ff061bc7d71e2098ec
SaiKasireddi <kasireddisaikumar@gmail.com>
2021-11-22 19:19:51 +0530

    Initial Commit

---
+++ SportsGear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SportsGear
+//
+//  Created by Sai Kumar Kasireddi's MacBook on 22/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SportsGear/SportsGearApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsGearApp.swift
+//  SportsGear
+//
+//  Created by Sai Kumar Kasireddi's MacBook on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SportsGearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
